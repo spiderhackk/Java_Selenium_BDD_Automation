1250def1b136eeff0137b499470ec412326b13e3
shilajit <sgpba2018@gmail.com>
2024-08-06 23:56:20 +0530

    added results folder

--- src/test/java/runner/CucumberTestNGRunnerTests.java
+++ src/test/java/runner/CucumberTestNGRunnerTests.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@SanityTest",features = {"src/test/resources/Dashboard.feature"},glue = {"definations"},plugin = {"pretty","html:target/testreport.html"})
+@CucumberOptions(tags = "@SanityTest",features = {"src/test/resources/Dashboard.feature"},glue = {"definations"},plugin = {"pretty","html:results/testreport.html"})
 public class CucumberTestNGRunnerTests extends AbstractTestNGCucumberTests {
 
 }
